b85281f8c084bc4814ad438a9c70547d36482eb4
aaronbean <aaron.bean@beardon.com>
2025-10-27 13:33:10 -0500

    allow for a completion text to be printed within the progress bar

--- presets/pac_man.js
+++ presets/pac_man.js
@@ -3,6 +3,8 @@ module.exports = {
     barChaser: 'ᗣ',
     barChaserColors: [ '#ffb7ff', '#ffb751', '#00ffff', '#ff0000' ],
     barCompleteCharacter: ' ',
+    barCompletionText: '<b>READY<i>!</i></b>',
+    barCompletionTextColor: '#ffff00',
     barGlue: '\u{1CC72}', // see https://www.unicode.org/L2/L2021/21235-terminals-supplement.pdf
     barGlueColor: '#ffff00',
     barIncompleteCharacter: '•',
--- presets/pac_man_legacy.js
+++ presets/pac_man_legacy.js
@@ -3,6 +3,8 @@ module.exports = {
     barChaser: 'ᗣ',
     barChaserColors: [ '#ffb7ff', '#ffb751', '#00ffff', '#ff0000' ],
     barCompleteCharacter: ' ',
+    barCompletionText: '<b>READY<i>!</i></b>',
+    barCompletionTextColor: '#ffff00',
     barGlue: 'ᗧ',
     barGlueColor: '#ffff00',
     barIncompleteCharacter: '•',
